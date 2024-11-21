e58de69dfcb59cad016c4e205a3fb449f6406633
YIJEUN <yijeun.dev@gmail.com>
2024-11-21 23:33:22 +0900

    [feat] Trigger Test
    Item / ItemBox / Mission Box 의 Trigger 기능 구현

---
+++ Assets/DEV/YJE/ResourceController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceController : MonoBehaviour
+{
+    public double maxHp;
+    public double curHp;
+
+    [SerializeField] GameObject itemPrefab;
+
+
+}
--- Assets/DEV/YJE/TestPlayer.cs
+++ Assets/DEV/YJE/TestPlayer.cs
@@ -1,8 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 public class TestPlayer : MonoBehaviour
 {
-    
+    private void OnTriggerStay(Collider other)
+    {
+        // 아이템과 트리거 발생 시
+        if(other.gameObject.tag == "Item")
+        {
+            Debug.Log("아이템 접촉");
+        }
+        // 아이템 박스와 트리거 발생 시
+        // TODO : Finish 태그를 ItemBox로 변경할 것
+        if (other.gameObject.tag == "Finish")
+        {
+            Debug.Log("아이템박스 접촉");
+        }
+        // 무기와 트리거 발생 시
+        if (other.gameObject.tag == "Weapon")
+        {
+            Debug.Log("무기 접촉");
+        }
+        // 미션 박스와 트리거 발생 시
+        // TODO : Finish 태그를 MissionBox로 변경할 것
+        if(other.gameObject.tag == "GameController")
+        {
+            Debug.Log("미션 박스 접촉");
+        }
+    }
 }
