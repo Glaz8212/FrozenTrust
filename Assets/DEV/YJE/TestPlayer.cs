7654cf079a5f73a741bca75b595e6780bcacc153
YIJEUN <yijeun.dev@gmail.com>
2024-11-22 16:34:57 +0900

    [feat] Resource ProtoType 구현
    1. Resoucrc의 ProtoType 구현
    - TakeDamage()함수를 구현하여 플레이어가 사용하도록 구현
    - Die() 함수를 구현하여 체력이 없는 경우 아이템을 드랍하고 사라지도록 구현
    - 일정 시간 후 리스폰 되도록 구현
    
    + 추후 인벤토리 기능 추가 시 함수의 세부 사항 수정 필요

--- Assets/DEV/YJE/TestPlayer.cs
+++ Assets/DEV/YJE/TestPlayer.cs
@@ -5,12 +5,31 @@ using static UnityEditor.Progress;
 
 public class TestPlayer : MonoBehaviour
 {
+    // 플레이어가 물체에 주는 데미지
+    [SerializeField] int damage;
     private void OnTriggerStay(Collider other)
     {
+
+        // 플레이어와 자원의 상호작용 테스트를 위한 임시 코드
+        if (other.gameObject.tag == "Resource")
+        {
+            Debug.Log("자원 충돌 발생");
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                other.gameObject.GetComponent<ResourceController>().TakeDamage(damage);
+            }
+        }
+
+
         // 아이템과 트리거 발생 시
-        if(other.gameObject.tag == "Item")
+        if (other.gameObject.tag == "Item")
         {
             Debug.Log("아이템 접촉");
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("아이템 줍기");
+                other.gameObject.GetComponent<ItemController>().SaveItem();
+            }
         }
         // 아이템 박스와 트리거 발생 시
         if (other.gameObject.tag == "ItemBox")
@@ -27,7 +46,7 @@ public class TestPlayer : MonoBehaviour
         {
             // TODO : 테스트를 위한 임의 함수
             Debug.Log("미션1 박스 접촉");
-            if (Input.GetKeyDown(KeyCode.F))
+            if (Input.GetKeyDown(KeyCode.E))
             {
                 other.gameObject.transform.GetComponentInParent<MissionController>().Mission1ClearChecked();
             }
@@ -37,7 +56,7 @@ public class TestPlayer : MonoBehaviour
         {
             // TODO : 테스트를 위한 임의 함수
             Debug.Log("미션2 박스 접촉");
-            if (Input.GetKeyDown(KeyCode.F))
+            if (Input.GetKeyDown(KeyCode.E))
             {
                 other.gameObject.transform.GetComponentInParent<MissionController>().Mission2ClearChecked();
             }
@@ -47,7 +66,7 @@ public class TestPlayer : MonoBehaviour
         {
             // TODO : 테스트를 위한 임의 함수
             Debug.Log("엔딩 탈출 포트 접촉");
-            if (Input.GetKeyDown(KeyCode.F))
+            if (Input.GetKeyDown(KeyCode.E))
             {
                 other.gameObject.transform.GetComponentInParent<MissionController>().EndingClearChecked();
             }
