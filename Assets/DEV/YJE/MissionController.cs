128a0e2e574bc9165271a862f78a9cffede28e6f
YIJEUN <yijeun.dev@gmail.com>
2024-11-22 11:52:21 +0900

    [feat] MissionController ProtoType 구현 + Tags 목록 수정
    1. MissionController.cs의 ProtoType 구현
    - Mission 기능 관련하여 클리어를 판단하는 함수 프로토타입 구현
    
    2. Tags 목록 갱신
    - Monster
    - ItemBox
    - Mission1
    - Mission2
    - Ending

---
+++ Assets/DEV/YJE/MissionController.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+
+public class MissionController : MonoBehaviour
+{
+    public bool Is1Clear = false;
+    public bool Is2Clear = false;
+    public bool IsEndingClear = false;
+
+    /// <summary>
+    /// Mission1의 클리어 여부를 확인하는 함수
+    /// </summary>
+    public void Mission1ClearChecked()
+    {
+        // 미션 클리어가 완료된 경우에는 함수 종료
+        if (Is1Clear)
+        {
+            Debug.Log("이미 1 클리어");
+            return;
+        }
+        // 미션1 클리어가 안된 경우
+        else
+        {
+            // TODO : 미션 아이템이 전부 들어갔는지 확인하여
+            //        들어간 경우 => 미션 클리어
+            //        부족한 경우 => 리턴
+            //        인벤토리 기능 추가 시 수정 필요
+            Debug.Log("1 클리어");
+            Is1Clear = true;
+        }
+    }
+
+    /// <summary>
+    /// Mission2의 클리어 여부를 확인하는 함수
+    /// </summary>
+    public void Mission2ClearChecked()
+    {
+        // 미션1 클리어가 완료되지 않은 경우에는 함수 종료
+        if (!Is1Clear)
+        {
+            Debug.Log("1 클리어 미완성");
+            return;
+        }
+        // 미션2 클리어가 완료된 경우 함수 종료
+        if (Is2Clear)
+        {
+            Debug.Log("이미 2 클리어");
+            return;
+        }
+        // 미션1 클리어가 된경우
+        else if (Is1Clear)
+        {
+            // TODO : 미션 아이템이 전부 들어갔는지 확인하여
+            //        들어간 경우 => 미션 클리어
+            //        부족한 경우 => 리턴
+            //        인벤토리 기능 추가 시 수정 필요
+            Debug.Log("2 클리어");
+            Is2Clear = true;
+        }
+    }
+
+    /// <summary>
+    /// 게임의 클리어 여부를 확인하는 함수
+    /// </summary>
+    public void EndingClearChecked()
+    {
+        // 미션1 또는 미션2가 클리어되지 않은 경우 함수 종료
+        if (!Is1Clear || !Is2Clear)
+        {
+            Debug.Log("1이나 2 클리어 미완성");
+            return;
+        }
+        // 미션1과 2가 모두 클리어 된 경우
+        else if (Is1Clear && Is2Clear)
+        {
+            Debug.Log("엔딩 클리어");
+            // 엔딩확인
+            IsEndingClear = true;
+        }
+    }
+
+}
--- Assets/DEV/YJE/TestPlayer.cs
+++ Assets/DEV/YJE/TestPlayer.cs
@@ -13,8 +13,7 @@ public class TestPlayer : MonoBehaviour
             Debug.Log("아이템 접촉");
         }
         // 아이템 박스와 트리거 발생 시
-        // TODO : Finish 태그를 ItemBox로 변경할 것
-        if (other.gameObject.tag == "Finish")
+        if (other.gameObject.tag == "ItemBox")
         {
             Debug.Log("아이템박스 접촉");
         }
@@ -24,10 +23,34 @@ public class TestPlayer : MonoBehaviour
             Debug.Log("무기 접촉");
         }
         // 미션 박스와 트리거 발생 시
-        // TODO : Finish 태그를 MissionBox로 변경할 것
-        if(other.gameObject.tag == "GameController")
+        if(other.gameObject.tag == "Mission1")
         {
-            Debug.Log("미션 박스 접촉");
+            // TODO : 테스트를 위한 임의 함수
+            Debug.Log("미션1 박스 접촉");
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                other.gameObject.transform.GetComponentInParent<MissionController>().Mission1ClearChecked();
+            }
+        }
+        // 미션 박스와 트리거 발생 시
+        if (other.gameObject.tag == "Mission2")
+        {
+            // TODO : 테스트를 위한 임의 함수
+            Debug.Log("미션2 박스 접촉");
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                other.gameObject.transform.GetComponentInParent<MissionController>().Mission2ClearChecked();
+            }
+        }
+        // 미션 박스와 트리거 발생 시
+        if (other.gameObject.tag == "Ending")
+        {
+            // TODO : 테스트를 위한 임의 함수
+            Debug.Log("엔딩 탈출 포트 접촉");
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                other.gameObject.transform.GetComponentInParent<MissionController>().EndingClearChecked();
+            }
         }
     }
 }
