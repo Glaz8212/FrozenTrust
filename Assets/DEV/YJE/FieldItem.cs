ca8c5a045bf7b035fda4a9f289607a9b4aac6365
YIJEUN <yijeun.dev@gmail.com>
2024-11-24 23:21:23 +0900

    [feat] Itme ObjectPool Prototype
    Itme의 ObjectPool의 기본 구성 설정

---
+++ Assets/DEV/YJE/FieldItem.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Purchasing;
+using UnityEngine;
+
+
+// 인벤토리 구현 연구 영상
+// https://youtu.be/74vxsqQsFHE?si=kpKFlGl9cDvYJsY4
+public class FieldItem : MonoBehaviour
+{
+    public Item item;
+    public SpriteRenderer image;
+
+    public void SetItme(Item setItem)
+    {
+        item.itemName = setItem.itemName;
+        item.itemImg = setItem.itemImg;
+        item.itemType = setItem.itemType;
+
+        image.sprite = item.itemImg;
+    }
+
+    /// <summary>
+    /// 아이템을 얻으면 사용
+    /// </summary>
+    /// <returns></returns>
+    public Item GetItem()
+    {
+        return item;
+    }
+
+}
