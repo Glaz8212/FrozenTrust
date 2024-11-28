deafd32db0e3e34c14b3604216ebe26ea94b47d0
jhs1998 <105158325+jhs1998@users.noreply.github.com>
2024-11-28 21:55:26 +0900

    [feat] 플레이어 상호작용 코드 문법 변경 및 최적화
    Null 조건연산자 로 null일 경우 대비
    OnTriggerEnter로 충돌 판정 변경
    삼항 연산자를 사용하여 코드 최적화

--- Assets/DEV/JHS/Scripts/PlayerInteraction.cs
+++ Assets/DEV/JHS/Scripts/PlayerInteraction.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 { 
-    public enum Type { Idle, Misson, ItemBox, Item }
+    public enum Type { Idle, Mission, ItemBox, Item }
     public Type type = Type.Idle;
     // 상호작용 상태 판정
     public bool isInteracting = false;
     // 콜라이더 충돌 판정
-    private bool isCollider = false;
+    private Collider currentCollider;
 
     public MissionBox missionController;
     public BoxController boxController;
@@ -39,7 +39,7 @@ public class PlayerInteraction : MonoBehaviour
     private void Update()
     {
         // E 키 입력 처리
-        if (Input.GetKeyDown(KeyCode.E) && !isInteracting && isCollider && status.playerDie == false)
+        if (Input.GetKeyDown(KeyCode.E) && !isInteracting && currentCollider != null && status.playerDie == false)
         {
             isInteracting = true; // 상호작용 중 상태로 변경
 
@@ -49,103 +49,86 @@ public class PlayerInteraction : MonoBehaviour
                 case Type.Idle:
                     Debug.Log("상호작용 가능한 상태가 아닙니다.");
                     break;
-                case Type.Misson:
-                    if (missionController != null)
-                    {
-                        missionController.MissionBoxOpen();
-                    }
-                    else
-                    {
-                        Debug.LogWarning("MissionBoxController 설정되지 않았습니다.");
-                    }
+                case Type.Mission:
+                    // null조건 연산자 ?. missionController의 값이 null이 아닐경우에만 MissionBoxOpen를 실행. nulld이면 MissionBoxOpen를 실행하지 않음
+                    missionController?.MissionBoxOpen();
                     break;
                 case Type.ItemBox:
-                    if (boxController != null)
-                    {
-                        boxController.BoxOpen();
-                    }
-                    else
-                    {
-                        Debug.LogWarning("BoxController가 설정되지 않았습니다.");
-                    }
+                    boxController?.BoxOpen();
                     break;
                 case Type.Item:
-                    if (item != null)
-                    {
-                        // 아이템 테스터의 interaction에 playerInventory를 넣어 실행
-                        item.interaction(playerInventory);
-                        type = Type.Idle;
-                        isCollider = false;
-                        isInteracting = false;
-                    }
-                    else
-                    {
-                        Debug.LogWarning("item이 설정되지 않았습니다.");
-                    }
+                    item?.interaction(playerInventory);
+                    ResetInteraction();
                     break;
             }
         }
         // 상호작용한 오브젝트에서 상호작용이 끝났을때 false값을 설정 한걸 가져와야됨
-        else if (!isCollider || (missionController != null && !missionController.IsUIOpen) || (boxController != null && !boxController.IsUIOpen))
-        {              
-            isInteracting = false;
+        if (missionController != null && !missionController.IsUIOpen || boxController != null && !boxController.IsUIOpen)
+        {
+            ResetInteraction();
         }
     }
 
    
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
+        // 콜라이더에 값이 들어가 있으면 리턴
+        if (currentCollider != null) return;
+
+        currentCollider = other;
+
         if (other.CompareTag("Mission1") || other.CompareTag("Mission2") || other.CompareTag("Ending"))
         {
             missionController = other.GetComponent<MissionBox>();
-            if (missionController != null)
-            {
-                type = Type.Misson;
-            }
+            // 삼항 연산자 사용 : 변수 = 조건문 ? 조건문이 참일 때 값 : 조건문이 거짓일 때 값
+            type = missionController != null ? Type.Mission : Type.Idle;
         }
         else if (other.CompareTag("ItemBox"))
         {
             boxController = other.GetComponent<BoxController>();
-            if (boxController != null)
-            {
-                type = Type.ItemBox;
-            }
+            // 타입이 boxController에 값이 있다면 ItemBox 아니면 Idle
+            type = boxController != null ? Type.ItemBox : Type.Idle;
         }
         else if (other.CompareTag("Item"))
         {
             item = other.GetComponent<Item>();
-            if (item != null)
-            {
-                type = Type.Item;
-            }
+            // 타입이 item에 값이 있으면 Item 없으면 Idle
+            type = item != null ? Type.Item : Type.Idle;
         }
         else
         {
             Debug.Log("상호작용 할수있는 오브젝트가 아닙니다");
             type = Type.Idle;
         }
-        isCollider = true;
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.GetComponent<MissionBox>() == missionController)
+        // 콜라이더에서 나온 값이 currentCollider 값과 같다면 실행 #땅 콜라이더가 이 값에 들어갈 경우가 있나?
+        if (other == currentCollider)
         {
-            missionController.MissionBoxClose();
-            missionController = null;
-        }
-        else if (other.GetComponent<BoxController>() == boxController)
-        {
-            boxController.BoxClose();
-            boxController = null;
-        }
-        else if (other.GetComponent<ItemController>() == item)
-        {
-            item = null;
-        }
+            // 미션 콜라이더에 값이 있고 그 값이 other의 MissionBox와 같다면 미션 ui닫기 실행
+            if (missionController != null && missionController == other.GetComponent<MissionBox>())
+            {
+                missionController.MissionBoxClose();
+            }
+            // 박스 콜라이더에 값이 있고 그 값이 other의 BoxController와 같다면 박스 ui닫기 실행
+            else if (boxController != null && boxController == other.GetComponent<BoxController>())
+            {
+                boxController.BoxClose();
+            }
 
+            ResetInteraction();
+        }
+    }
+
+    private void ResetInteraction()
+    {
         type = Type.Idle;
-        isCollider = false; // 충돌 상태 해제
-        isInteracting = false; // 상호작용 상태 초기화
+        currentCollider = null;
+        missionController = null;
+        boxController = null;
+        item = null;
+        isInteracting = false;
     }
 }
