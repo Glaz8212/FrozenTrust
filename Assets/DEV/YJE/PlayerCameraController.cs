70954d70ebe8e752570708cef5b79b890e9c7866
YIJEUN <yijeun.dev@gmail.com>
2024-11-28 13:42:48 +0900

    [feat] Camera의 Player 참조 오류 수정
    Camera의 Player 참조 오류 수정

--- Assets/DEV/YJE/PlayerCameraController.cs
+++ Assets/DEV/YJE/PlayerCameraController.cs
@@ -5,18 +5,17 @@ using UnityEngine;
 
 public class PlayerCameraController : MonoBehaviourPunCallbacks
 {
-    public CinemachineVirtualCamera cinemachineVirtualCamera; // 시네머신 카메라
+    private CinemachineVirtualCamera cinemachineVirtualCamera;
     private GameSceneManager gameSceneManager;
+    public GameObject nowPlayer;
 
     private void Awake()
     {
         cinemachineVirtualCamera = GetComponent<CinemachineVirtualCamera>();
+        gameSceneManager = GameObject.Find("GameSceneManager").GetComponent<GameSceneManager>();
     }
 
-    /// <summary>
-    /// 방에 입장 시 실행
-    /// </summary>
-    public override void OnJoinedRoom()
+    private void Start()
     {
         StartCoroutine(StartDelayRoutine());
     }
@@ -28,13 +27,14 @@ public class PlayerCameraController : MonoBehaviourPunCallbacks
     IEnumerator StartDelayRoutine()
     {
         // 네트워크 준비에 필요한 시간 설정
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(3f);
         SetCam();
     }
 
     private void SetCam()
     {
-        cinemachineVirtualCamera.Follow = gameSceneManager.nowPlayer.transform;
-        cinemachineVirtualCamera.LookAt = gameSceneManager.nowPlayer.transform;
+        nowPlayer = gameSceneManager.nowPlayer;
+        cinemachineVirtualCamera.Follow = nowPlayer.transform;
+        cinemachineVirtualCamera.LookAt = nowPlayer.transform;
     }
 }
