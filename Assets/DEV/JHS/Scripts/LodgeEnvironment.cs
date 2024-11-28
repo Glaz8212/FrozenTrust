7271007afbd3070c5913a3579e29282c2efdcd6c
jhs1998 <105158325+jhs1998@users.noreply.github.com>
2024-11-28 11:52:48 +0900

    [feat] 플레이어 환경 변경
    산장에 출입 시 플레이어의 상태가 warm일 경우 cold로 cold일 경우 warm으로 변경

---
+++ Assets/DEV/JHS/Scripts/LodgeEnvironment.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static PlayerStatus;
+
+public class LodgeEnvironment : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerStatus playerStatus = other.GetComponent<PlayerStatus>();
+            if (playerStatus != null)
+            {
+                if (playerStatus.environment == SurroundingEnvironment.Cold)
+                {
+                    playerStatus.ChangeEnvironment(SurroundingEnvironment.Warm);
+                }
+                else if (playerStatus.environment == SurroundingEnvironment.Warm)
+                {
+                    playerStatus.ChangeEnvironment(SurroundingEnvironment.Cold);
+                }
+            }
+        }
+    }
+}
