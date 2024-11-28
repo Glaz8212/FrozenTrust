14188d27c385196c2003381b49022ce5235f303c
jhs1998 <105158325+jhs1998@users.noreply.github.com>
2024-11-28 11:55:25 +0900

    [fix] 거점환경 스크립트 이름 변경

--- Assets/DEV/JHS/Scripts/BaseEnvironment.cs
+++ Assets/DEV/JHS/Scripts/BaseEnvironment.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using static PlayerStatus;
 
-public class LodgeEnvironment : MonoBehaviour
+public class BaseEnvironment : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
