94eb98e32d37d9c794a235b251ebe775032ef347
loadingim <68083530+loadingim@users.noreply.github.com>
2024-11-29 11:26:27 +0900

    [Update] Item
    해당 오브젝트의 소유권이 없어서 삭제가 되지않아
    Item스크립트에서 RPC함수로 마스터 클라이언트에 삭제 요청

--- Assets/DEV/LSH/Scripts/Item.cs
+++ Assets/DEV/LSH/Scripts/Item.cs
@@ -18,9 +18,38 @@ public class Item : MonoBehaviour
 
     public void interaction(PlayerInventory playerInventory)
     {
+
         playerInventory.AddItem(itemName, itemSprite, itemCount);
 
-        PhotonNetwork.Destroy(gameObject);
+        PhotonView photonView = GetComponent<PhotonView>();
+
+        if(photonView != null)
+        {
+            // RPC 마스터 클라이언트에 ViewID 전송
+            photonView.RPC(nameof(DestroyItem), RpcTarget.MasterClient, photonView.ViewID);
+        }
+        
+        //PhotonNetwork.Destroy(gameObject);
         // Destroy(gameObject);
+    }
+
+    [PunRPC]
+    public void DestroyItem(int viewID)
+    {
+        // 마스터 클라이언트에 삭제를 요청
+        if (PhotonNetwork.IsMasterClient)
+        {
+            // 포톤뷰 ID를 기반으로 오브젝트 검색
+            PhotonView view = PhotonView.Find(viewID);
+            if (view != null)
+            {
+                PhotonNetwork.Destroy(view.gameObject);
+                Debug.Log($"아이템이 정상적으로 삭제되었습니다.");
+            }
+            else
+            {
+                Debug.LogError($"{viewID}를 찾을 수 없습니다.");
+            }
+        }
     }
 }
